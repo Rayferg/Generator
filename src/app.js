4f8b834fe6a4b4bc3629211d48e9c21822e01dda
Rayferg <Rayferg19@gmail.com>
2022-10-16 00:46:07 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+function excusegen() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".io", ".us", ".net"];
+  let array = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let t = 0; t < adj.length; t++) {
+      for (let p = 0; p < noun.length; p++) {
+        for (let o = 0; o < dom.length; o++) {
+          array.push(pronoun[i] + adj[t] + noun[p] + dom[o]);
+        }
+      }
+    }
+  }
+  return array;
+}
+let generator = excusegen();
+for (let k = 0; k < generator.length; k++) {
+  document.querySelector("#js").innerHTML += "<p>" + generator[k] + "</p>";
+}
